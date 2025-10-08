d848e7caf1cc3b75055f74855481cbdffeadd803
Ryan Bui <z5421437@ad.unsw.edu.au>
2025-10-08 12:08:50 +1100

    Added check-circles and text input for reminders

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { reminder } from "./reminders.js"
 
 console.log("Hello World");
 
-const add_reminder_button = document.getElementById("plus");
+const add_reminder_button = document.getElementById("plus-button");
 add_reminder_button.addEventListener("click", (e) => {
     reminder();
 });
--- src/reminders.js
+++ src/reminders.js
@@ -4,15 +4,31 @@ export function reminder() {
     reminder.classList.add("reminder");
 
     // Create Check-circle portion of the reminder
-    const check_circle = document.createElement("button");
+    const check_circle = document.createElement("label");
     check_circle.classList.add("check-circle");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox_input";
+    check_circle.appendChild(checkbox);
+
+    const checkmark = document.createElement("span");
+    checkmark.classList.add("checkmark");
+    check_circle.appendChild(checkmark);
+
     reminder.appendChild(check_circle);
 
     // Create text portion of the reminder 
-    const text_container = document.createElement("div");
-    text_container.classList.add("reminder-text");
-    text_container.textContent = "Placeholder";
-    reminder.appendChild(text_container);
+    const text_label = document.createElement("label");
+    text_label.classList.add("reminder-label");
+
+    const text_input = document.createElement("input");
+    text_input.type = "text";
+    text_input.id = "text-input";
+    text_input.classList.add("box");
+    text_label.appendChild(text_input);
+    reminder.appendChild(text_label);
 
     reminders_list.appendChild(reminder);
+    text_input.focus();
 }
